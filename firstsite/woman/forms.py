80592ec95f4585ef3a87bfe1bb6cf3e59ad83a35
Kostiantyn Kokhan <kohan.xxxxx@gmail.com>
2023-01-22 23:55:49 +0100

    Login functional commit.

--- firstsite/woman/forms.py
+++ firstsite/woman/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import *
 from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.forms import User
 
 
@@ -35,6 +35,11 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
     # title = forms.CharField(max_length=255, label="Name", widget=forms.TextInput(attrs={'class': 'form-input'}))
     # slug = forms.SlugField(max_length=255, label="URL", widget=forms.TextInput(attrs={'class': 'form-input'}))
--- firstsite/woman/views.py
+++ firstsite/woman/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from django.urls import reverse_lazy
+from django.contrib.auth import logout, login
+
 
 from .forms import *
 from .models import *
@@ -51,10 +55,6 @@ def contact(request):
     return HttpResponse('Contact')
 
 
-def login(request):
-    return HttpResponse('Login')
-
-
 class ShowPost(DataMixin, DetailView):
     model = Woman
     template_name = 'woman/post.html'
@@ -99,5 +99,29 @@ class RegisterUser(DataMixin, CreateView):
         context_def = self.get_user_context(title='Register')
         context.update(context_def)
         return context
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'woman/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context_def = self.get_user_context(title='Login')
+        context.update(context_def)
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
 
 
